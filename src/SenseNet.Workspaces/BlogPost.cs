477836ee3cb369fa03b060c1984902b89f2679e9
tusmester <tusmester@gmail.com>
2017-05-19 18:07:39 +0200

    Add Workspaces .Net project for code behind and UI-related classes. Current dependency: SenseNet.Services.

---
+++ src/SenseNet.Workspaces/BlogPost.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using SenseNet.ContentRepository.Schema;
+using SenseNet.ContentRepository;
+using SenseNet.ContentRepository.Storage;
+
+namespace SenseNet.Portal
+{
+    [ContentHandler]
+    public class BlogPost : GenericContent
+    {
+        public BlogPost(Node parent) : this(parent, null) { }
+        public BlogPost(Node parent, string nodeTypeName) : base(parent, nodeTypeName) { }
+        protected BlogPost(NodeToken nt) : base(nt) { }
+
+        public const string PUBLISHEDON = "PublishedOn";
+        [RepositoryProperty(PUBLISHEDON, RepositoryDataType.DateTime)]
+        public DateTime PublishedOn
+        {
+            get { return this.GetProperty<DateTime>(PUBLISHEDON); }
+            set { base.SetProperty(PUBLISHEDON, value); }
+        }
+
+        public override void Save(NodeSaveSettings settings)
+        {
+            base.Save(settings);
+            this.MoveToFolder();
+        }
+        private void MoveToFolder()
+        {
+            DateTime pubDate;
+            if (!DateTime.TryParse(this[PUBLISHEDON].ToString(), out pubDate))
+                return;
+
+            var dateFolderName = $"{pubDate.Year}-{pubDate.Month:00}";
+
+            // check if the post is already in the proper folder
+            if (this.ParentName == dateFolderName) return;
+
+            // check if the proper folder exists
+            var targetPath = RepositoryPath.Combine(this.WorkspacePath, string.Concat("Posts/", dateFolderName));
+            if (!Node.Exists(targetPath))
+            {
+                // target folder needs to be created
+                Content.CreateNew("Folder", Node.LoadNode(RepositoryPath.Combine(this.WorkspacePath, "Posts")), dateFolderName).Save();
+            }
+
+            // hide this move from journal
+            this.NodeOperation = ContentRepository.Storage.NodeOperation.HiddenJournal;
+
+            // move blog post to the proper folder
+            this.MoveTo(Node.LoadNode(targetPath));
+        }
+        
+        public override object GetProperty(string name)
+        {
+            switch (name)
+            {
+                case PUBLISHEDON:
+                    return this.PublishedOn;
+                default:
+                    return base.GetProperty(name);
+            }
+        }
+        public override void SetProperty(string name, object value)
+        {
+            switch (name)
+            {
+                case PUBLISHEDON:
+                    this.PublishedOn = (DateTime)value;
+                    break;
+                default:
+                    base.SetProperty(name, value);
+                    break;
+            }
+        }
+    }
+}
---
+++ src/SenseNet.Workspaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+#if DEBUG
+[assembly: AssemblyTitle("SenseNet.Workspaces (Debug)")]
+#else
+[assembly: AssemblyTitle("SenseNet.Workspaces (Release)")]
+#endif
+
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sense/Net Inc.")]
+[assembly: AssemblyCopyright("Copyright © Sense/Net Inc.")]
+[assembly: AssemblyProduct("sensenet ECM")]
+[assembly: AssemblyTrademark("Sense/Net Inc.")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("7.0.0.0")]
+[assembly: AssemblyFileVersion("7.0.0.0")]
+[assembly: AssemblyInformationalVersion("7.0.0-beta1")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("2cc1585f-5a27-4205-987f-727c4db503f9")]
