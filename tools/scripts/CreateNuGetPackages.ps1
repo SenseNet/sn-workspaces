5a274830fe63688c4fa2301b08462e109aea9985
tusmester <tusmester@gmail.com>
2017-07-21 15:06:44 +0200

    Merge changes from release branch before release beta2.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -1,4 +1,7 @@
 ﻿Compress-Archive -Path "..\..\src\nuget\snadmin\install-workspaces\*" -Force -CompressionLevel Optimal -DestinationPath "..\..\src\nuget\content\Admin\tools\install-workspaces.zip"
+
+nuget pack ..\..\src\SenseNet.Workspaces\Workspaces.nuspec -properties Configuration=Release
 nuget pack ..\..\src\SenseNet.Workspaces\Workspaces.Install.nuspec -properties Configuration=Release
 
-# nuget.exe push -Source "SenseNet" -ApiKey VSTS SenseNet.Workspaces.Install.7.0.0-beta1.nupkg
+# nuget.exe push -Source "SenseNet" -ApiKey VSTS SenseNet.Workspaces.7.0.0-beta2.nupkg
+# nuget.exe push -Source "SenseNet" -ApiKey VSTS SenseNet.Workspaces.Install.7.0.0-beta2.nupkg
