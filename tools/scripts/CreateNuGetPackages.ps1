88c112539e29aed113165c4093936551c2138e7f
tusmester <tusmester@gmail.com>
2017-08-04 15:59:29 +0200

    Fix: create folder during nuget package creation correctly.

--- tools/scripts/CreateNuGetPackages.ps1
+++ tools/scripts/CreateNuGetPackages.ps1
@@ -1,10 +1,16 @@
 ﻿$srcPath = [System.IO.Path]::GetFullPath(($PSScriptRoot + '\..\..\src'))
-$installPackagePath = "$srcPath\nuget\content\Admin\tools\install-workspaces.zip"
+$installPackageFolder = "$srcPath\nuget\content\Admin\tools"
+$installPackagePath = "$installPackageFolder\install-workspaces.zip"
 
 # delete existing packages
 Remove-Item $PSScriptRoot\*.nupkg
 
-Compress-Archive -Path "..\..\src\nuget\snadmin\install-workspaces\*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePath
+if (!(Test-Path $installPackageFolder))
+{
+	New-Item $installPackageFolder -Force -ItemType Directory
+}
+
+Compress-Archive -Path "$srcPath\nuget\snadmin\install-workspaces\*" -Force -CompressionLevel Optimal -DestinationPath $installPackagePath
 
 nuget pack $srcPath\SenseNet.Workspaces\Workspaces.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
 nuget pack $srcPath\SenseNet.Workspaces\Workspaces.Install.nuspec -properties Configuration=Release -OutputDirectory $PSScriptRoot
