d00659752d392a467c522c04e827ad7e0bb80bd6
tusmester <tusmester@gmail.com>
2018-02-06 15:47:21 +0100

    Update Services reference to version 7.1 (IR rewrite).

--- src/SenseNet.Workspaces/Properties/AssemblyInfo.cs
+++ src/SenseNet.Workspaces/Properties/AssemblyInfo.cs
@@ -15,9 +15,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("sensenet ECM")]
 [assembly: AssemblyTrademark("Sense/Net Inc.")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("7.0.0.0")]
-[assembly: AssemblyFileVersion("7.0.0.0")]
-[assembly: AssemblyInformationalVersion("7.0.0")]
+[assembly: AssemblyVersion("7.1.0.0")]
+[assembly: AssemblyFileVersion("7.1.0.0")]
+[assembly: AssemblyInformationalVersion("7.1.0")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("2cc1585f-5a27-4205-987f-727c4db503f9")]
